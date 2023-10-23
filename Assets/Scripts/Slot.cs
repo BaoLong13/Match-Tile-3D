1c6d55d5c77fcafafdf04d6055a755ba62273e0c
Do DInh Bao Long <madtrickster13@gmail.com>
2023-10-23 17:06:04 +0700

    Revert "Revert "+Add Gameplay mechanic and UI""
    This reverts commit 043c068658005254c9e59797b60271bf0e69e8ae.

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,10 @@ using UnityEngine;
 public class Slot : MonoBehaviour
 {
     public Tile occupiedTile;
-    public bool isEmpty = true;
+    public bool isEmpty;
+
+    private void Start()
+    {
+        isEmpty = true;
+    }
 }
